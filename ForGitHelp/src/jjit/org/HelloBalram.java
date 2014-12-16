1d7b531285ebb376e7fea5d139e8a23992435bbd
BalRamDixit <balram.dixit@gmail.com>
2014-12-16 16:57:28 +0530

    first time commit

---
+++ ForGitHelp/src/jjit/org/HelloBalram.java
@@ -0,0 +1,10 @@
+package jjit.org;
+
+public class HelloBalram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Helloo Git World");
+	}
+
+}
